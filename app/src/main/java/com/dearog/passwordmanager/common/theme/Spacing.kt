b30d34c76792d21e9ce8024614c09591fa0984de
r3911221-rgb <r3911221@gmail.com>
2026-07-07 13:42:40 +0530

    Create app/src/main/java/com/dearog/passwordmanager/common/theme/Spacing.kt

---
+++ app/src/main/java/com/dearog/passwordmanager/common/theme/Spacing.kt
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2026 Dearog Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.dearog.passwordmanager.common.theme
+
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+/**
+ * Standard spacing values used throughout the application.
+ *
+ * Avoid hardcoded dp values in UI. Use AppSpacing instead.
+ */
+object AppSpacing {
+
+    /** 0dp */
+    val None: Dp = 0.dp
+
+    /** 2dp */
+    val ExtraExtraSmall: Dp = 2.dp
+
+    /** 4dp */
+    val ExtraSmall: Dp = 4.dp
+
+    /** 8dp */
+    val Small: Dp = 8.dp
+
+    /** 12dp */
+    val MediumSmall: Dp = 12.dp
+
+    /** 16dp */
+    val Medium: Dp = 16.dp
+
+    /** 20dp */
+    val MediumLarge: Dp = 20.dp
+
+    /** 24dp */
+    val Large: Dp = 24.dp
+
+    /** 32dp */
+    val ExtraLarge: Dp = 32.dp
+
+    /** 40dp */
+    val ExtraExtraLarge: Dp = 40.dp
+
+    /** 48dp */
+    val Huge: Dp = 48.dp
+
+    /** 56dp */
+    val TouchTarget: Dp = 56.dp
+
+    /** 64dp */
+    val ScreenMargin: Dp = 64.dp
+}
