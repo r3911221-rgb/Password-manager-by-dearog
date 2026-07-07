a2ce5052fbf4627ef1d8c610ec4e23ab9aa3f219
r3911221-rgb <r3911221@gmail.com>
2026-07-07 13:50:53 +0530

    Create app/src/main/java/com/dearog/passwordmanager/common/components/AppIconButton.kt

---
+++ app/src/main/java/com/dearog/passwordmanager/common/components/AppIconButton.kt
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2026 Dearog Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.dearog.passwordmanager.common.components
+
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import com.dearog.passwordmanager.common.theme.AppDimensions
+
+/**
+ * Standard icon button used throughout the application.
+ */
+@Composable
+fun AppIconButton(
+    icon: ImageVector,
+    contentDescription: String?,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true
+) {
+    IconButton(
+        onClick = onClick,
+        modifier = modifier.size(AppDimensions.ButtonHeight),
+        enabled = enabled,
+        colors = IconButtonDefaults.iconButtonColors()
+    ) {
+        Icon(
+            imageVector = icon,
+            contentDescription = contentDescription,
+            modifier = Modifier.size(AppDimensions.IconMedium),
+            tint = MaterialTheme.colorScheme.onSurface
+        )
+    }
+}
