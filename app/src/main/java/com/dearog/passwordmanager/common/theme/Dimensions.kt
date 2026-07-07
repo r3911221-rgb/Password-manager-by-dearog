44eac1d3d6af623a9a34631e7f2aaa434c68e6a1
r3911221-rgb <r3911221@gmail.com>
2026-07-07 13:43:28 +0530

    Create app/src/main/java/com/dearog/passwordmanager/common/theme/Dimensions.kt

---
+++ app/src/main/java/com/dearog/passwordmanager/common/theme/Dimensions.kt
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2026 Dearog Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.dearog.passwordmanager.common.theme
+
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+/**
+ * Common UI dimensions shared across the application.
+ *
+ * Use these values instead of hardcoded dimensions.
+ */
+object AppDimensions {
+
+    /* Icons */
+
+    val IconExtraSmall: Dp = 12.dp
+    val IconSmall: Dp = 16.dp
+    val IconMedium: Dp = 24.dp
+    val IconLarge: Dp = 32.dp
+    val IconExtraLarge: Dp = 48.dp
+
+    /* Buttons */
+
+    val ButtonHeight: Dp = 56.dp
+    val ButtonMinWidth: Dp = 120.dp
+
+    /* Text Fields */
+
+    val TextFieldHeight: Dp = 56.dp
+
+    /* App Bar */
+
+    val TopAppBarHeight: Dp = 64.dp
+    val BottomBarHeight: Dp = 80.dp
+
+    /* Navigation */
+
+    val NavigationRailWidth: Dp = 80.dp
+    val NavigationDrawerWidth: Dp = 320.dp
+
+    /* FAB */
+
+    val FabSize: Dp = 56.dp
+    val SmallFabSize: Dp = 40.dp
+
+    /* Cards */
+
+    val CardMinHeight: Dp = 72.dp
+
+    /* Dialog */
+
+    val DialogMaxWidth: Dp = 560.dp
+
+    /* Dividers */
+
+    val DividerThickness: Dp = 1.dp
+
+    /* Border */
+
+    val BorderThin: Dp = 1.dp
+    val BorderMedium: Dp = 2.dp
+
+    /* Progress */
+
+    val ProgressIndicatorStroke: Dp = 4.dp
+}
