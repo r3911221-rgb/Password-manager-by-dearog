87988ff3a1287225ec178bf8c52badfc8d000f59
r3911221-rgb <r3911221@gmail.com>
2026-07-07 13:43:59 +0530

    Create app/src/main/java/com/dearog/passwordmanager/common/theme/Theme.kt

---
+++ app/src/main/java/com/dearog/passwordmanager/common/theme/Theme.kt
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2026 Dearog Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.dearog.passwordmanager.common.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+/**
+ * Main application theme.
+ *
+ * This is the single entry point for the application's
+ * Material 3 theme configuration.
+ */
+@Composable
+fun DearogTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+
+    val colorScheme = if (darkTheme) {
+        DarkColorScheme
+    } else {
+        LightColorScheme
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = AppTypography,
+        shapes = AppShapes,
+        content = {
+            Surface(
+                modifier = Modifier,
+                color = MaterialTheme.colorScheme.background,
+                contentColor = MaterialTheme.colorScheme.onBackground
+            ) {
+                content()
+            }
+        }
+    )
+}
